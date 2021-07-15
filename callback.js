059bf962af0cf3cc6c37f34cc43706b4337f3339
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-07-15 11:38:12 +0200

    callback

---
+++ callback.js
@@ -0,0 +1,22 @@
+function makeUppercase(value) {
+  console.log(value.toUpperCase())
+}
+// makeUppercase('peter')
+
+function reverseString(value) {
+  console.log(value.split('').reverse().join(''))
+}
+
+function handleName(name, cb) {
+  const fullName = `${name} smith`
+  cb(fullName)
+}
+// handleName("peter", makeUppercase);
+// handleName("peter", reverseString);
+
+handleName("susan", (value) => console.log(value));
+
+const btn = document.querySelector(".btn");
+btn,addEventListener("click", function(){
+  console.log("hello world! ");
+})
